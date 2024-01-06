1cb4712374654da61433c003e8033880cc2cdbe7
Berkayyildirim24 <90174527+Berkayyildirim24@users.noreply.github.com>
2024-01-06 19:25:36 +0300

    Fizyoterapist_web_sitesi
    Html, Css, Bootstrap, Js kullanarak geliştirdiğim web sitesi

---
+++ onehealth/js/dosya.js
@@ -0,0 +1,4 @@
+function aramayiBaslat() {
+    var telefonNumarasi = "0536 997 73 85"; // Telefon numarasını buraya yazın veya değişkene atayın
+    window.location.href = "tel:" + telefonNumarasi;
+  }
